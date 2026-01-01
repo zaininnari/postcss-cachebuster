d9c0e75149b6f11124835a9c415ead41f799accd
zaininnari <zaininnari@gmail.com>
2026-01-01 21:36:38 +0900

    feat[eslint]: add

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import n from "eslint-plugin-n";
+import {defineConfig} from "eslint/config";
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                ...globals.node,
+                ...globals.mocha,
+                myCustomGlobal: "readonly",
+            },
+        },
+        plugins: {js, n},
+        extends: ["js/recommended", "n/recommended"],
+    },
+]);
